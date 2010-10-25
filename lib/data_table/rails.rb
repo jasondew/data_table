7dff997e52c8d3c2b6e93fc8bdb0cce61ec74243
Jason Dew <jason.dew@gmail.com>
2010-10-25 11:24:21 -0400

    fixing bug loading AR

--- lib/data_table/rails.rb
+++ lib/data_table/rails.rb
@@ -2,8 +2,8 @@ module DataTable
   class Railtie < Rails::Railtie
     initializer 'data_table.initialize' do
       ActiveSupport.on_load(:active_record) do
-        ActiveRecord::Base.send :extend, DataTable::ClassMethods
-        ActiveRecord::Base.send :extend, DataTable::ActiveRecord::ClassMethods
+        ::ActiveRecord::Base.send :extend, DataTable::ClassMethods
+        ::ActiveRecord::Base.send :extend, DataTable::ActiveRecord::ClassMethods
       end
     end
   end
