948b7df74264267ac5419923693197e6b5283f8a
Jason Dew <jason.dew@gmail.com>
2011-10-23 17:16:12 -0400

    fixing issue where data_table was tricking other gems into thinking Mongoid was loaded

--- lib/mongoid/data_table.rb
+++ lib/mongoid/data_table.rb
@@ -1,8 +1,10 @@
-module Mongoid
-  module DataTable
-    def self.included base
-      base.send :extend, ::DataTable::ClassMethods
-      base.send :extend, ::DataTable::Mongoid::ClassMethods
+if defined?(Mongoid)
+  module Mongoid
+    module DataTable
+      def self.included base
+        base.send :extend, ::DataTable::ClassMethods
+        base.send :extend, ::DataTable::Mongoid::ClassMethods
+      end
     end
   end
 end
