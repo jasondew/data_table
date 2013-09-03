457f9030dc7de8696de3bb3902e2deb22ca3acec
Marcel <poeppe@gmx.li>
2013-09-03 23:21:32 +0200

    add support for single column search

--- lib/data_table/active_record.rb
+++ lib/data_table/active_record.rb
@@ -3,7 +3,7 @@ module DataTable
     module ClassMethods
 
       def _find_objects params, fields, search_fields
-        self.where(_where_conditions params[:ssearch], search_fields).
+        self.where(_build_conditions params, search_fields).
              includes(_discover_joins fields).
              order(_order_fields params, fields).
              paginate :page => _page(params), :per_page => _per_page(params)
@@ -27,6 +27,36 @@ module DataTable
 
         joins.to_a
       end
+			
+			def _build_conditions params, search_fields, join_operator = "OR"
+				search_params = _search_params params
+				return if search_params.empty?
+				all_conditions = []
+				all_parameters = []
+				search_params.each do |key,value|
+					next if value[:ssearch].blank?
+					value[:ssearch].split.each do |term|
+						next if (search_fields[value[:mdataprop].to_i]).nil?
+						next if (clause = _where_condition(term, search_fields[value[:mdataprop].to_i].dup)).empty?
+						all_conditions << [clause.shift]
+						all_parameters += [clause]
+					end
+				end	
+				if !params[:ssearch].blank?
+					params[:ssearch].split.each do |term|
+						conditions = []
+						parameters = []						
+						search_fields.each do |field|
+							next if (clause = _where_condition(term, field.dup)).empty?
+							conditions << clause.shift
+							parameters += clause
+						end
+						all_conditions << conditions
+						all_parameters << parameters		
+					end
+				end
+				[all_conditions.map {|conditions| "(" + conditions.join(" #{join_operator} ") + ")" }.join(" AND "), *all_parameters.flatten]
+			end
 
       def _where_conditions query, search_fields, join_operator = "OR"
         return if query.blank?
@@ -104,6 +134,25 @@ module DataTable
         direction = params[:ssortdir_0] == "asc" ? "ASC" : "DESC"
         %{#{fields[params[:isortcol_0].to_i]} #{direction}}
       end
+			
+			def _search_params params
+				return if params.empty?
+				search_params = ["bsortable","mdataprop","ssearch"]
+				search = {}
+				params.each do |key,value|
+					parts = key.to_s.split('_')
+					if parts.length==2
+						index = parts[1].to_i
+						if search_params.include?(parts[0])
+							if search[index].nil?
+								search[index] = {}
+							end
+							search[index].merge!(parts[0].to_sym => value)
+						end
+					end
+				end
+				search
+			end				
     end
   end
 end
